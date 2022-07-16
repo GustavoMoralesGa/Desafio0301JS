f2b457dfa7796a4df56d28723fabd045c43a49d6
GustavoMoralesGa <gustavo.morales.ga@gmail.com>
2022-07-16 19:09:34 -0400

    [ADD] new changes, add html, falta detalles y agregar cantidad y color

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,14 @@
+// STEP 1
 
-titulo = document.querySelector("h1")
-titulo.style.color = "#7ba238"
+cantidad = document.querySelector('#quantity');
+inputcolor = document.querySelector('#inputColor');
+price = document.querySelector('#price');
+
+// STEP 2
+
+totalPrice = document.querySelector('#total');
+totalQuantity = document.querySelector('#totalQuantity');
+totalColor = document.querySelector('#totalColor')
+
+totalPrice.innerHTML = Number(quantity.value) * Number(quantity.innerHTML);
+totalQuantity.innerHTML = quantity.value; 
